20cc6bb30210453e8932a564efda8738e16b5d1f
Kratika177 <108918796+Kratika177@users.noreply.github.com>
2022-07-25 14:38:50 +0530

    Add files via upload

---
+++ chapter 18 with open block.py
@@ -0,0 +1,5 @@
+with open("sample.txt") as f:
+    print(f.readlines())
+#this is same as open and close file in this we donot need to close the file explicitly it will closed automatically.
+#f=open("sample.txt")
+#f.close()
